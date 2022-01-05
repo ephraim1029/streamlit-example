a5f771e27fe46d860d79b15c04eb3abc29ca1080
Bijay <59042448+B8484@users.noreply.github.com>
2022-01-05 13:47:02 +0630

    streamlit.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,17 @@
-import pandas as pd
-import numpy as np
-import pandas_datareader as web
-import datetime as dt
- 
- 
-start = dt.datetime(2020,1,1)
-end = dt.datetime.now()
- 
-df = web.DataReader('XRP-USD', 'yahoo', start, end)
-df
+import streamlit as st
+name = st.text_input("Enter Your name", "Type Here ...")
+result = name.title()
+st.success(result)
+
+status = st.radio("Select Gender: ", ('Male', 'Female'))
+
+if (status == 'Male'):
+    st.success("Male")
+else:
+    st.success("Female")
+
+ hobby = st.selectbox("Hobbies: ",
+                     ['Dancing', 'Reading', 'Sports'])
+ st.write("Your hobby is: ", hobby)
+
+
