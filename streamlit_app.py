4a5f82d4320c12266b4ae8148a188fd20a132516
Bijay <59042448+B8484@users.noreply.github.com>
2022-02-02 19:59:31 +0630

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,9 +58,7 @@ test_data_recall_score3 = recall_score(Y_test, X_test_prediction3)
 import streamlit as st
 input_data = st.text_input('CSV file path')
 input_data = input_data.split(",")
-float(input_data)
 tuple(input_data)
-len(input_data)
 
 input_data_as_numpy_array = np.asarray(input_data)
 
