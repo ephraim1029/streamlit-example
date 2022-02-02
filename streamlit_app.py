a17267b375227ee3ee33b429dc6489c1a816f162
Bijay <59042448+B8484@users.noreply.github.com>
2022-02-02 19:27:25 +0630

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,10 +56,9 @@ test_data_precision3  = precision_score(Y_test, X_test_prediction3)
 test_data_recall_score3 = recall_score(Y_test, X_test_prediction3)
 
 import streamlit as st
-input_data = st.text_input('CSV file path')
-int(input_data)
-float(input_data)
+input_data = st.text_input(float('CSV file path'))
 input_data = input_data.split(",")
+
 tuple(input_data)
 len(input_data)
 input_data_as_numpy_array = np.asarray(input_data)
